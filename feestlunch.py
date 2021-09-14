d9a5cc55dae3e0e469caf62e0fb8c334296975c7
Jsyr963 <89968301+Jsyr963@users.noreply.github.com>
2021-09-14 11:03:35 +0200

    Eerste commit!

---
+++ feestlunch.py
@@ -0,0 +1,10 @@
+aantalcroissantjes = 17
+prijscroissant = 0.39
+
+aantalstokbrood = 2
+prijsstokbrood = 2.78
+
+aantalkortingsbonnen = 3
+korting = 0.50
+
+print(aantalcroissantjes*prijscroissant + aantalstokbrood*prijsstokbrood - aantalkortingsbonnen-korting)
---
+++ speelhaldag.py
@@ -0,0 +1,9 @@
+aantaltickets = 3
+prijsticket = 7.45
+
+prijsper5min = 0.37
+aantalkeer = 9
+aantalpersonen = 3
+
+
+print((aantaltickets*prijsticket)+prijsper5min*aantalkeer*aantalpersonen)
